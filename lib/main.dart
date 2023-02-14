00b2291790935d17a0b5b62e1308da7a095d1261
TetRey <reyhaneldwin31@gmail.com>
2023-02-14 21:12:39 +0700

    Appbar sudah selesai

--- lib/main.dart
+++ lib/main.dart
@@ -16,9 +16,17 @@ class MyApp extends StatelessWidget {
           backgroundColor: Color(0xffF3654B),
           toolbarHeight: 60,
           elevation: 10,
-          title: Image.asset(
-            "images/stock fm.png",
-            fit: BoxFit.cover,
+          title: Padding(
+            padding: EdgeInsets.fromLTRB(12, 0, 12, 0),
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Image.asset("images/stock fm.png"),
+                  Image.asset(
+                    "images/Profile-icon.png",
+                    width: 36,
+                  )
+                ]),
           ),
         ),
       ),
